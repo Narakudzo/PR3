2eb5b6167865c2745efebd60a8901ad63ce41cb5
Kutabare <70647725+Narakudzo@users.noreply.github.com>
2021-10-20 09:52:28 +0300

    Тестирование первой функции прошло успешно

--- test_main.py
+++ test_main.py
@@ -9,14 +9,3 @@ class UnitTest(unittest.TestCase):
         a1 = main
         self.assertEqual(a1.work_distribution_of_leaflets(300), 307)
 
-
-class Test(TestCase):
-    def test_work_programming(self):
-        a2 = main
-        self.assertEqual(a2.work_programming(200), 215)
-
-if __name__ == "__main__":
-    u = UnitTest()
-    u.test_work_distribution_of_leaflets()
-    u2 = Test()
-    u2.test_work_programming()
